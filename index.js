0dd4dbdccd2f204559bd92960789b5b99fefbe4b
bradydowling <bradydowling@gmail.com>
2020-05-07 08:51:48 -0400

    Remove unused data and code

--- index.js
+++ index.js
@@ -1,87 +1,25 @@
 const puppeteer = require('puppeteer');
-const fs = require('fs');
+const baseConferenceUrl = 'https://www.churchofjesuschrist.org/general-conference?lang=eng';
 
-async function getAnimals(animalType) {
-    let animalPath;
-    if (animalType === 'fish') {
-        animalPath = 'Fish_(New_Leaf)';
-    }
-    else if (animalType === 'bug') {
-        animalPath = 'Bugs_(New_Leaf)';
-    }
-    else if (animalType === 'deepSeaCreature') {
-        animalPath = 'Deep-sea_creatures';
-    }
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto(`https://animalcrossing.fandom.com/wiki/${animalPath}`);
+async function getTalkSources(talkUrl) {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(talkUrl);
 
-    const fishData = await page.evaluate(() => {
-        const camelize = (str) => {
-            return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(word, index) {
-                return index === 0 ? word.toLowerCase() : word.toUpperCase();
-            }).replace(/\s+/g, '');
-        };
-        const header = [...document.querySelectorAll('table thead tr')][0];
-        const headerItemsWithMonths = [...header.querySelectorAll('th')].map(node => camelize(node.innerText.trim()));
-        const months = headerItemsWithMonths.filter(item => item.length === 3);
-        const headerItems = headerItemsWithMonths.filter(item => item.length !== 3);
-        const fishRows = [...document.querySelector('tr tbody').querySelectorAll('tr')];
-        const allFishData = fishRows.map((tr, i) => {
-            const thisFish = [...tr.querySelectorAll('td')].reduce((fishData, td, i) => {
-                const monthStartIndex = headerItems.indexOf('time') + 1;
-                if (i < monthStartIndex) {
-                    const headerKey = headerItems[i];
-                    if (i === 1) {
-                        fishData[headerKey] = td.querySelector('img').src;
-                    }
-                    else {
-                        fishData[headerKey] = td.innerText;
-                    }
-                    return fishData;
-                }
-                if (td.innerText === '✓') {
-                    fishData.months.push(months[i - monthStartIndex]);
-                }
-                return fishData;
-            }, { months: [] });
-            return thisFish;
-        });
+  const talkSources = await page.evaluate(() => {
+    return [];
+  });
 
-        return allFishData;
-    });
-
-    await browser.close();
-    return fishData;
+  await browser.close();
+  return talkSources;
 }
 
-async function getAllAnimalData() {
-    const fishData1 = await getAnimals('fish');
-    const fishData = fishData1.map((fish) => {
-        return {
-            ...fish,
-            animalType: 'fish'
-        }
-    });
-    const bugsData1 = await getAnimals('bug');
-    const bugsData = bugsData1.map((bug) => {
-        return {
-            ...bug,
-            animalType: 'bug'
-        }
-    });
-    const deepSeaCreatures1 = await getAnimals('deepSeaCreature');
-    const deepSeaCreatures = deepSeaCreatures1.map((deepSeaCreature) => {
-        return {
-            ...deepSeaCreature,
-            animalType: 'deepSeaCreature'
-        }
-    });
-    return [...fishData, ...bugsData, ...deepSeaCreatures];
+async function getAllConferenceTalkData() {
+  const talkUrl = 'https://www.churchofjesuschrist.org/study/general-conference/2020/04/16holmes?lang=eng';
+  const talkSources = await getTalkSources(talkUrl);
+  return talkSources;
 }
 
-getAllAnimalData().then((animals) => {
-    fs.writeFile('animals.json', JSON.stringify(animals, null, 2), () => {
-        console.log('written!');
-    });
+getAllConferenceTalkData().then((talkSources) => {
+  console.log(talkSources);
 });
