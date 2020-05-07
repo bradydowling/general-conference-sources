5afd6fd0564297777168e19780fc0525b188d76e
bradydowling <bradydowling@gmail.com>
2020-05-07 09:18:05 -0400

    Preliminary untested code

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 const puppeteer = require('puppeteer');
 const baseConferenceUrl = 'https://www.churchofjesuschrist.org/general-conference?lang=eng';
 
-async function getTalkSources(talkUrl) {
+function parseTalkSources(talkData) {
+  return [];
+}
+
+async function getTalkData(talkUrl) {
+  const talkBaseUrl = 'https://www.churchofjesuschrist.org/study/';
+  const talkDataPath = talkUrl.split(talkBaseUrl);
+  const talkDataUrlBase = 'https://www.churchofjesuschrist.org/study/api/v3/language-pages/type/content?lang=eng&uri=';
+  const talkDataUrl = `${talkDataUrlBase}${talkDataPath}`;
+  fetch(talkDataUrl)
+    .then(response => {
+      return response.json();
+    });
+}
+
+async function getAllConferenceTalkSources(baseConferenceUrl) {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto(talkUrl);
+  await page.goto(baseConferenceUrl);
 
-  const talkSources = await page.evaluate(() => {
+  const talkUrls = await page.evaluate(() => {
     return [];
   });
+
+  const talkSources = await talkUrls.map(getTalkData);
 
   await browser.close();
   return talkSources;
 }
 
-async function getAllConferenceTalkData() {
-  const talkUrl = 'https://www.churchofjesuschrist.org/study/general-conference/2020/04/16holmes?lang=eng';
-  const talkSources = await getTalkSources(talkUrl);
-  return talkSources;
-}
-
-getAllConferenceTalkData().then((talkSources) => {
+getAllConferenceTalkSources(baseConferenceUrl).then((talkSources) => {
   console.log(talkSources);
 });
